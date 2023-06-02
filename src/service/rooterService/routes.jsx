2dc353167cedbcca49373eaf06dbfed41fe61635
Fatih Gürses <73244604+grs21@users.noreply.github.com>
2023-06-02 11:37:04 +0300

    router

---
+++ src/service/rooterService/routes.jsx
@@ -0,0 +1,14 @@
+import TestPage from "../../pages/MainPage/testPage"
+import VideoCall from "../../pages/MainPage/videocall"
+
+export const routes = [
+
+    {
+        path: '/',
+        component: TestPage,
+    },
+    {
+        path:'/videoCall',
+        component:VideoCall
+    }
+]
